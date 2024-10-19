1495c3b0abeaedf9c7db99d5ffd18e8f9fb08d6b
Albert <diaz.albertjohn123@gmail.com>
2024-10-19 14:48:56 +0800

    commit example

--- app/src/main/java/com/example/finaleditprofile/profile_main/edit_profile.kt
+++ app/src/main/java/com/example/finaleditprofile/profile_main/edit_profile.kt
@@ -1,13 +1,22 @@
 package com.example.finaleditprofile.profile_main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageButton
 import com.example.finaleditprofile.R
 
 class EditProfileActivity: AppCompatActivity() {
+    private lateinit var SaveChangesButton: AppCompatImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.anader_edit_profile) // Use the layout file for editing profile
+
+        SaveChangesButton = findViewById<AppCompatImageButton>(R.id.btnSaveChanges)
+
+        SaveChangesButton.setOnClickListener {
+            val intent = Intent(this, ProfilePage::class.java)
+            startActivity(intent)
     }
 }
